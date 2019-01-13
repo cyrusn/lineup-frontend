ce7db24dd70640455b96ff5201c199efb3544266
cyrusn <cyrusncy@gmail.com>
2019-01-13 12:31:41 +0800

    fix width

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,40 @@
 <template>
   <div>
     <div class="container mt-3">
-      <div class="w-50 mx-auto">
-        <h1 class="display-4 text-center">家長日接見系統</h1>
-        <hr>
-        <div v-if="errorMessage" class="alert alert-danger">
-          {{errorMessage}}
+      <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6 align-self-center">
+          <h1 class="display-4 text-center">家長日接見系統</h1>
+          <hr>
+          <div v-if="errorMessage" class="alert alert-danger">
+            {{errorMessage}}
+          </div>
+          <form @keypress.enter='onLogin'>
+            <div class="form-group">
+              <select class="form-control" v-model='page'>
+                <option value='' disabled>選擇工作</option>
+                <option v-for='r in routers' :key='r.id' :value='r.path'>{{r.name}}</option>
+              </select>
+            </div>
+            <floor-selector />
+            <div class='form-group'
+              v-if="page === '/interview-room' && currentFloor !== ''">
+              <select class="form-control" v-model='clazz' @change='updateClazz(clazz)'>
+                <option disabled value="">選擇班別</option>
+                <option
+                  v-for='c in floorClazzes' :key='c.id'
+                  :value="c.classcode">{{c.classcode}}
+                </option>
+              </select>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" id="username" placeholder="登入名稱" v-model='userAlias'>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" id="password" placeholder="密碼" v-model='password'>
+            </div>
+            <button type="button" class="btn btn-primary" @click="onLogin">遞交</button>
+          </form>
         </div>
-        <form @keypress.enter='onLogin'>
-          <div class="form-group">
-            <select class="form-control" v-model='page'>
-              <option value='' disabled>選擇工作</option>
-              <option v-for='r in routers' :key='r.id' :value='r.path'>{{r.name}}</option>
-            </select>
-          </div>
-          <floor-selector />
-          <div class='form-group'
-            v-if="page === '/interview-room' && currentFloor !== ''">
-            <select class="form-control" v-model='clazz' @change='updateClazz(clazz)'>
-              <option disabled value="">選擇班別</option>
-              <option
-                v-for='c in floorClazzes' :key='c.id'
-                :value="c.classcode">{{c.classcode}}
-              </option>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" id="username" placeholder="登入名稱" v-model='userAlias'>
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" id="password" placeholder="密碼" v-model='password'>
-          </div>
-          <button type="button" class="btn btn-primary" @click="onLogin">遞交</button>
-        </form>
       </div>
     </div>
   </div>
